21dd1a035c2db963bd60a9dd376517df9de32ef0
Andrew Nalley <awnalley@gmail.com>
2023-09-27 20:03:21 -0500

    add bank class

---
+++ src/bank.java
@@ -0,0 +1,9 @@
+public class bank {
+    public static void main(String[] args) throws Exception {
+        Account andrew = new Account("Andrew Bird", "A00001");
+		
+		
+		Account frank = new Account("Frank Sinatra", "F00002");
+		frank.showMenu();
+    }
+}
